af250940d9476bebb0121e6e427d26ad93ddc8ae
潇军 <http://git.aihuoshi.net/huangxj>
2023-04-12 14:09:53 +0800

    <feat>:集成vitepress

---
+++ docs/index.md
@@ -0,0 +1,9 @@
+<!--
+ * @Author: XiaoJun
+ * @Date: 2023-04-12 14:00:02
+ * @LastEditors: XiaoJun
+ * @LastEditTime: 2023-04-12 14:03:06
+ * @Description: 组件功能
+ * @FilePath: /xj-utils/docs/index.md
+-->
+### 你看看我
---
+++ index.ts
@@ -0,0 +1,11 @@
+/**
+ * @Author: XiaoJun
+ * @Date: 2023-04-12 14:05:58
+ * @LastEditors: XiaoJun
+ * @LastEditTime: 2023-04-12 14:06:03
+ * @Description: 组件功能
+ * @FilePath: /xj-utils/index.ts
+ */
+export default {
+  xjStartFlag: '我开始了'
+};
